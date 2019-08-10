cd3230f0de01aff2baacf167bb2f15eb31b2098b
mithrandir15 <davido0960@gmail.com>
2019-08-10 17:12:17 -0400

    initial searchBar logic

--- app/src/main/java/com/example/dolomitev2/SearchFragment.java
+++ app/src/main/java/com/example/dolomitev2/SearchFragment.java
@@ -3,6 +3,7 @@ package com.example.dolomitev2;
 import android.animation.Animator;
 import android.animation.AnimatorInflater;
 import android.animation.AnimatorListenerAdapter;
+import android.app.Activity;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
@@ -10,16 +11,24 @@ import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
+import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.Toast;
 
 
 public class SearchFragment extends Fragment {
     LinearLayout LinearSearchContainer;
+    ListView stockList;
+    EditText searchBar;
+    String searchTerm;
 
     public SearchFragment() {
         // Required empty public constructor
@@ -32,7 +41,45 @@ public class SearchFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_search, container, false);
         getActivity().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
         LinearSearchContainer = view.findViewById(R.id.LinearSearchContainer);
+        searchBar = view.findViewById(R.id.searchSearchBar);
+        stockList = view.findViewById(R.id.searchStockList);
+        searchBar.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View view, int i, KeyEvent keyEvent) {
+                if ((keyEvent.getAction() == KeyEvent.ACTION_DOWN) &&
+                        (i == KeyEvent.KEYCODE_ENTER)) {
+                    searchTerm = searchBar.getText().toString();
+                    Log.d("searchTerm", searchTerm);
+                    hideKeyboard(view);
+                    return true;
+                }
+                return false;
+            }
+        }
+        );
+
+        searchBar.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean hasFocus) {
+                if (! hasFocus) {
+                    hideKeyboard(view);
+                }
+            }
+        });
+
+        stockList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+            }
+        });
+
         return view;
+    }
+
+    private void hideKeyboard(View view) {
+        InputMethodManager inputMethodManager = (InputMethodManager) getActivity().getSystemService(Activity.INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
     }
 
     /*
